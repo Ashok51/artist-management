0baaa8e92dd9fbc54ee8f913f418071996cbb48b
Ashok Poudel <ashokpoudel2051@gmail.com>
2024-04-01 22:12:38 +0545

    create raw view for artist and music creation using stimulus dynamic forms and controller using ORM

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,49 @@
 class ArtistsController < ApplicationController
   def index
+    @artists = Artist.all
   end
 
   def new
+    @artist = Artist.new
+    @artist.musics.build
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      flash[:notice] = "Artist Created Successfully."
+      redirect_to @artist
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    if @artist.update(artist_params)
+      flash[:notice] = "Artist updated successfully."
+      redirect_to @artist
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name, :date_of_birth,
+                                    :gender, :address, :first_release_year,
+                                    :no_of_albums_released,
+                                    musics_attributes: %i[
+                                      id title album_name genre _destroy
+                                    ])
   end
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -5,5 +5,7 @@ class Artist < ApplicationRecord
                  female: 'female',
                  other: 'other' }
 
-  validates :name, :date_of_birth, :gender, :address, :first_released_year, presence: true
+  validates :name, :date_of_birth, :address, :first_release_year, presence: true
+
+  accepts_nested_attributes_for :musics, allow_destroy: true
 end
