9ebe0ef6194f329c7c5961c092a58a42188e98bb
Ashok Poudel <ashokpoudel2051@gmail.com>
2024-04-06 07:54:40 +0545

    hide nav item

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :first_name, :last_name, :phone, :date_of_birth, :gender, :address, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "artists#index"
+  root "added_users#index"
 
   resources :artists do
     collection do
