5abcf0e662afd38204a880e5e3816586f4611c44
Ashok Poudel <ashokpoudel2051@gmail.com>
2024-04-06 23:49:40 +0545

    refactor codebase

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,21 +9,10 @@ module Admin
     include UsersSqlHandler
     require_relative '../concerns/sql_queries'
 
+    rescue_from ActiveRecord::StatementInvalid, with: :handle_database_error
+
     def index
-      ActiveRecord::Base.transaction do
-        @page_number = params[:page].to_i || 1
-        per_page = 5
-
-        @total_pages = total_page_of_user_table(per_page)
-
-        query = SQLQueries::ORDERD_USERS_RECORD
-
-        result = Pagination.paginate(query, @page_number, per_page)
-
-        @admin_users = User.build_user_objects_from_json(result)
-      end
-    rescue ActiveRecord::StatementInvalid => e
-      Rails.logger.error("Error while fetching users: #{e.message}")
+      fetch_users
     end
 
     def new
@@ -32,67 +21,90 @@ module Admin
 
     def create
       ActiveRecord::Base.transaction do
-        generated_password = Devise.friendly_token.first(8)
-        user_hash = create_new_user(generated_password)
-        @admin_user = User.new(user_hash.first)
-
-        # Send welcome email
-        UserMailer.welcome_email(@admin_user, generated_password).deliver_now
+        create_user_with_email
       end
       redirect_to admin_users_path, notice: 'User created successfully.'
-    rescue ActiveRecord::StatementInvalid => e
-      Rails.logger.error("Error while creating user: #{e.message}")
-
-      render :new
     end
 
     def show
-      user_hash = show_user
-
-      @user = User.new(user_hash.first)
+      fetch_user
     end
 
     def edit; end
 
     def update
       ActiveRecord::Base.transaction do
-        update_user
+        update_user_record
       end
       redirect_to admin_users_url, notice: 'User updated successfully.'
-    rescue ActiveRecord::StatementInvalid => e
-      Rails.logger.error("Error updating user: #{e.message}")
-
-      redirect_to admin_users_url, alert: 'Unable to update user.'
     end
 
     def destroy
       ActiveRecord::Base.transaction do
-        delete_user
+        delete_user_record
       end
       redirect_to admin_users_url, notice: 'User deleted successfully.'
-    rescue ActiveRecord::StatementInvalid => e
-      Rails.logger.error("Error while deleting user: #{e.message}")
-
-      redirect_to admin_users_url, alert: 'Unable to delete user.'
     end
 
     private
 
-    def generated_params_with_password(generated_password)
-      user_params.merge(password: generated_password,
-                        password_confirmation: generated_password)
+    def fetch_users
+      @page_number = params[:page].to_i || 1
+      per_page = 5
+
+      @total_pages = total_page_of_user_table(per_page)
+
+      query = SQLQueries::ORDERD_USERS_RECORD
+
+      result = Pagination.paginate(query, @page_number, per_page)
+
+      @admin_users = User.build_user_objects_from_json(result)
+    end
+
+    def create_user_with_email
+      generated_password = Devise.friendly_token.first(8)
+      user_hash = create_new_user(generated_password)
+      @admin_user = User.new(user_hash.first)
+
+      # Send welcome email
+      UserMailer.welcome_email(@admin_user, generated_password).deliver_now
+    end
+
+    def fetch_user
+      user_hash = show_user
+      @user = User.new(user_hash.first)
+    end
+
+    def update_user_record
+      update_user
+    end
+
+    def delete_user_record
+      delete_user
     end
 
     def set_user
       @admin_user = User.find(params[:id])
     end
 
-    def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :phone, :date_of_birth, :gender, :address)
-    end
-
     def sanitize_gender
       params[:user][:gender] = User.genders[params[:user][:gender].to_sym]
+    end
+
+    def user_params
+      params.require(:user)
+            .permit(:first_name, :last_name, :email, :phone, :date_of_birth, :gender, :address)
+    end
+
+    def generated_params_with_password(generated_password)
+      user_params.merge(password: generated_password,
+                        password_confirmation: generated_password)
+    end
+
+    def handle_database_error(exception)
+      Rails.logger.error("Database error: #{exception.message}")
+      flash.now[:alert] = 'Database operation failed.'
+      render :new
     end
   end
 end
--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,24 +1,19 @@
 # frozen_string_literal: true
 
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: %i[show edit update destroy]
+  before_action :set_page_number, only: [:index]
+
   require 'csv'
-  include ArtistMusicCreation
+  include ArtistMusicSqlHandler
   include DatabaseExecution
 
   require_relative './concerns/sql_queries'
 
   def index
-    @artists = []
-    @page_number = params[:page].to_i || 1
-    per_page = 5 # Set the number of records per page
-
+    per_page = 5
     @total_pages = total_page_of_artist_table(per_page)
-
-    query = SQLQueries::ORDER_ARTIST_RECORD
-
-    result = Pagination.paginate(query, @page_number, per_page)
-
-    @artists = Artist.build_artist_object_from_json(result)
+    @artists = paginate_artists(per_page)
   end
 
   def new
@@ -29,25 +24,16 @@ class ArtistsController < ApplicationController
   def create
     ActiveRecord::Base.transaction do
       artist_id = create_artist_record
-
       create_music_of_artist(artist_id)
     end
     redirect_to artists_url, notice: 'Artist created successfully.'
   rescue ActiveRecord::StatementInvalid => e
-    Rails.logger.error("Error while creating artist: #{e.message}")
-
-    redirect_to artists_url, alert: 'Unable to create artist.'
+    handle_error(e, 'Unable to create artist.')
   end
 
-  def show
-    artist_hash = show_artist
+  def show; end
 
-    @artist = Artist.new(artist_hash.first)
-  end
-
-  def edit
-    show
-  end
+  def edit; end
 
   def update
     ActiveRecord::Base.transaction do
@@ -55,9 +41,7 @@ class ArtistsController < ApplicationController
     end
     redirect_to artists_url, notice: 'Artist updated successfully.'
   rescue ActiveRecord::StatementInvalid => e
-    Rails.logger.error("Error updating artist: #{e.message}")
-
-    redirect_to artists_url, alert: 'Unable to update artist.'
+    handle_error(e, 'Unable to update artist.')
   end
 
   def destroy
@@ -66,31 +50,43 @@ class ArtistsController < ApplicationController
     end
     redirect_to artists_url, notice: 'Artist deleted successfully.'
   rescue ActiveRecord::StatementInvalid => e
-    Rails.logger.error("Error while deleting artist: #{e.message}")
-
-    redirect_to artists_url, alert: 'Unable to delete artist.'
+    handle_error(e, 'Unable to delete artist.')
   end
 
   def import
     CsvImportService.import_artists_and_musics(params[:file])
     redirect_to artists_url, notice: 'Artists and Musics imported successfully.'
   rescue StandardError => e
-    Rails.logger.error("Error while importing artist: #{e.message}")
-
-    redirect_to artists_url, alert: 'Unable to import artists.'
+    handle_error(e, 'Unable to import artists.')
   end
 
   def export
     csv_data = CsvExportService.export_artists_and_musics
-
     send_data csv_data, filename: 'artists_with_musics.csv'
   rescue StandardError => e
-    Rails.logger.error("Error while exporting artists: #{e.message}")
-
-    redirect_to artists_url, alert: 'Unable to export the artists.'
+    handle_error(e, 'Unable to export the artists.')
   end
 
   private
+
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
+
+  def set_page_number
+    @page_number = params[:page].to_i || 1
+  end
+
+  def handle_error(exception, message)
+    Rails.logger.error("Error: #{exception.message}")
+    redirect_to artists_url, alert: message
+  end
+
+  def paginate_artists(per_page)
+    query = SQLQueries::ORDER_ARTIST_RECORD
+    result = Pagination.paginate(query, @page_number, per_page)
+    Artist.build_artist_object_from_json(result)
+  end
 
   def artist_params
     params.require(:artist).permit(:name, :date_of_birth,
