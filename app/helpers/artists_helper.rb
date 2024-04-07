8fa596947bf1749c02c3fd0836db175bfcc23585
Ashok Poudel <ashokpoudel2051@gmail.com>
2024-04-07 20:42:52 +0545

    take string value while importing csv and bug in prompt of artist and music

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -8,4 +8,12 @@ module ArtistsHelper
   def genre_options_for_select
     Music.genres.map { |key, value| [key, value] }
   end
+
+  def selected_artist_gender(gender)
+    Artist.genders[gender]
+  end
+
+  def selected_music_genre(genre)
+    Music.genres[genre]
+  end
 end
